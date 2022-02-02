43579997328cd9bbf2a6b975e5a25ad270dfab70
roshkadima <97698518+roshkadima@users.noreply.github.com>
2022-02-02 14:41:12 +0200

    Add files via upload

---
+++ Homework/home work.py
@@ -0,0 +1,7 @@
+#exercise 5
+
+
+
+s = input()
+s = s.split()
+print(max(s, key=len))
---
+++ Homework/main.py
@@ -0,0 +1,17 @@
+#exercise 1
+
+x = input('x =')
+print(x.count('b'))
+
+print('================================')
+#exercise 2
+
+
+
+
+#exercise 3
+print('================================')
+
+print(sum(map(ord, input())))
+
+
